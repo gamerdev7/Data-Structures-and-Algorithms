1bf8842ba9e63ffe5bb0e7c903e5b34ff7c4a7cf
= <=>
2022-01-06 17:13:05 +0530

    Insertion Sort

---
+++ Insertion Sort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new[] { 2, 4, 5, 1, 3 };
+            InsertionSort(a);
+
+            foreach (var item in a)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        public static void InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                var temp = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && temp < array[j])
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
